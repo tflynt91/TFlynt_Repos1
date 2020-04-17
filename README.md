64b2e472815cafcb89c0e353bd2c883c97ab8feb
tflynt91 <flynt.tyler@gmail.com>
2020-04-17 14:12:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TFlynt_Repos
---
+++ countingCharacters/countingCharacters.js
@@ -0,0 +1,20 @@
+function countingCharacters(stringToCount) {
+    // The length property has been mentioned in 
+    // the Logging Letter Exercise in Lesson 4
+    console.log(stringToCount + " has " + 
+              stringToCount.length + " characters.");
+}
+
+function countingCharacters2(stringToCount, characterToFind){
+    var characterCount = 0;
+    for (var characterPosition = 0;
+         characterPosition < stringToCount.length;
+         characterPosition++){
+        if(stringToCount[characterPosition] == characterToFind){
+            characterCount++;
+        }
+    }
+    console.log("String to search in: " + stringToCount);
+    console.log("Character to find: " + characterToFind);
+    console.log("Number of times character appears: " + characterCount);
+}
